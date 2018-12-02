3679c7ecece561e707623ac9ee3a33b72023ded0
agajka <a.klijewska@gmail.com>
2018-12-02 01:32:48 +0100

    third commit

--- Strings_formating.py
+++ Strings_formating.py
@@ -30,9 +30,29 @@ print("| {:6.3f} | {:16s} | {:10s} |".format(9.69, "Yohan Blake", "23.09.2012"))
 print("| {:6.3f} | {:16s} | {:10s} |".format(9.745, "Asafa Powell", "2.09.2008"))
 print("-" * szer)
 
-"""
+
 
 waluta = "dolar"
 us = 1
 pln = 4.08234915
-print("Aktualnie {:d} {} kosztuje {:.2f} zł".format(us, waluta, pln))
+print("Aktualnie {} {} kosztuje {:.2f} zł".format(us, waluta, pln))
+
+
+
+print("{} ma {}".format("Ala", "kota"))
+print("{1} ma {0}".format("Ala", "kota"))
+
+
+cm = float(input("Ile chciałbyś mieć centymetrów? ",))
+print("Czy jesteś pewien, że chciałbyś mieć {:.4f} metrów, czyli {:.4f} cali?".format(cm/100,cm*0.39370))
+
+"""
+
+print("Witaj w kalkulatorze do obliczania stopy zwrotu z lokaty, uwzględniający miesięczną kapitalizację odsetek.")
+sp = float(input("\nPodaj stan początkowy konta: "))
+opr = float(input("Podaj stopę oprocentowania rocznego [%]: "))
+dl = float(input("Podaj długosc lokaty w latach: "))
+
+zysk = sp*(1+((opr/100)*0.81)/12)**(12*dl)
+
+print("\nTwoje {:} złotych przez {:.0f} lat(a) na lokacie z {:} % oprocentowaniem urosnie do {:.2f} (uwzględniając podatek Belki)".format(sp, dl, opr, zysk))
